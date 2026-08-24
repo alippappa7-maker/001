6dc82d29d82213bdcd67b842e01d562a9df4908f
alippappa7-maker <alippappa7@gmail.com>
2026-08-24 10:14:08 +0300

    0

--- update_settings_viewmodel.py
+++
@@ -1,6 +0,0 @@
-import re
-
-with open('app/src/main/java/com/example/ui/SettingsViewModel.kt', 'r') as f:
-    content = f.read()
-
-# I need to know the contents first! Let's just output the contents of SettingsViewModel.kt instead of writing it blindly.
